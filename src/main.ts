eba574d1447ff6b9f159bc192a5dd6ef965f60a2
romazherdev <romazherdev@gmail.com>
2022-06-25 12:43:44 +0200

    three.js basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import './style.css'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-const app = document.querySelector<HTMLDivElement>('#app')!
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xf6edee);
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, 45, 65);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+renderer.outputEncoding = THREE.sRGBEncoding;
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const axesHelper = new THREE.AxesHelper(5);
+scene.add(axesHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  renderer.render(scene, camera);
+}
+
+animate();
